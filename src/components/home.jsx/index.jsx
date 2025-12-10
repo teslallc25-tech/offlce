a542716a3c28cc9f18c31415cf393f3e7b46139e
teslallc25-tech <teslallc25@gmail.com>
2025-12-10 13:17:30 -0500

    just do it

--- src/components/home.jsx/index.jsx
+++ src/components/home.jsx/index.jsx
@@ -29,10 +29,8 @@ function Home(){
                             Sign in
                         </button>
                     </Link>
-                    <Link to='/'>
-                       <button className='w-[200px]  h-fit'>Get Microsoft 365</button>
-                    </Link>
-                    <Link to='/adminlogin' className='hiddenp' >
+                    
+                    <Link to='/adminlogin' >
                        <button className='w-[200px]  h-fit'>Get Microsoft 365</button>
                     </Link>
                 </div>
